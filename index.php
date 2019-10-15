8bbf9d7b4ffa6e469c02d19f601d4d41258584b6
AngieSuarez97 <49985007+AngieSuarez97@users.noreply.github.com>
2019-10-15 16:40:18 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*Con esta instrucción le digo al servidor que rediriga  
+ * mi pagina web a la carpeta public 
+ * (donde tendre mi landing page...y cualquiera tendra acceso)*/
+header("location: public/");
+?>
+
